9128b97e632ab13ff246336c5f363ea93692e304
michaelbannach <michaelbannach@gmx.de>
2025-12-17 22:07:44 +0100

    Add PdfAttachmentRepository to CaseFlow.Infrastructure

---
+++ CaseFlow.Infrastructure/Repositories/PdfAttachmentRepository.cs
@@ -0,0 +1,43 @@
+using CaseFlow.Application.Interfaces;
+using CaseFlow.Domain.Models;
+using CaseFlow.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaseFlow.Infrastructure.Repositories;
+
+public class PdfAttachmentRepository : IPdfAttachmentRepository
+{
+    private readonly AppDbContext _db;
+
+    public PdfAttachmentRepository(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<List<PdfAttachment>> GetByFormCaseIdAsync(int formCaseId)
+    {
+        return await _db.PdfAttachments
+            .AsNoTracking()
+            .Where(a => a.FormCaseId == formCaseId)
+            .OrderByDescending(a => a.UploadedAt)
+            .ToListAsync();
+    }
+
+    public async Task<PdfAttachment?> GetByIdAsync(int id)
+    {
+        return await _db.PdfAttachments
+            .FirstOrDefaultAsync(a => a.Id == id);
+    }
+
+    public async Task<bool> AddAsync(PdfAttachment attachment)
+    {
+        _db.PdfAttachments.Add(attachment);
+        return await _db.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> DeleteAsync(PdfAttachment attachment)
+    {
+        _db.PdfAttachments.Remove(attachment);
+        return await _db.SaveChangesAsync() > 0;
+    }
+}
