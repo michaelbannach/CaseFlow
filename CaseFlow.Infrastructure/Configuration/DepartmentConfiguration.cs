2494c221cfc48ad4ea7d2750b21d18a53007b60e
michaelbannach <michaelbannach@gmx.de>
2025-12-31 15:40:54 +0100

    Add department configuration and database migrations

--- CaseFlow.Infrastructure/Configuration/DepartmentConfiguration.cs
+++ CaseFlow.Infrastructure/Configuration/DepartmentConfiguration.cs
@@ -17,5 +17,13 @@ public class DepartmentConfiguration : IEntityTypeConfiguration<Department>
         
         builder.HasIndex(d => d.Name)
             .IsUnique();
+        
+        builder.HasData(
+            new Department { Id = 1, Name = "Allgemein" },
+            new Department { Id = 2, Name = "Leistungen" },
+            new Department { Id = 3, Name = "Kosten" },
+            new Department { Id = 4, Name = "Organisation" }
+        );
+        
     }
 }
