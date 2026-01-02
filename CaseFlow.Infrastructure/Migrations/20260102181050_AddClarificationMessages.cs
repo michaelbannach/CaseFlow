dc6b5b0843120c6af52f1d996e2c2dd1a576d9f2
michaelbannach <michaelbannach@gmx.de>
2026-01-03 00:42:33 +0100

    Add clarification messages migration

---
+++ CaseFlow.Infrastructure/Migrations/20260102181050_AddClarificationMessages.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CaseFlow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClarificationMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FormCases_Employees_ProcessingEmployeeId",
+                table: "FormCases");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FormCases_Employees_ProcessingEmployeeId",
+                table: "FormCases",
+                column: "ProcessingEmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FormCases_Employees_ProcessingEmployeeId",
+                table: "FormCases");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FormCases_Employees_ProcessingEmployeeId",
+                table: "FormCases",
+                column: "ProcessingEmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id");
+        }
+    }
+}
